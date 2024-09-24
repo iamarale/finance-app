c931390f02ec8c2c4676ca608f628dab86d50c64
iamarale <ismailarales@gmail.com>
2024-09-23 23:18:08 -0500

    chore: cleaned supabase code and made helper function

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -5,4 +5,13 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // export client api
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export const getSupabaseData = async (table: string) => {
+  const { data, error } = await supabase.from(table).select();
+
+  // Throws error message
+  if (error) throw new Error(error.message);
+  // Returns data from table
+  return data;
+};
