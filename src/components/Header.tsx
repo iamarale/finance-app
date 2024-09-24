3758117cd41826bfb95d70f9d4540042304d50f2
iamarale <ismailarales@gmail.com>
2024-09-23 22:25:06 -0500

    style: responsive header with searched created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,97 @@
+// imports
 import { Link } from "@tanstack/react-router";
-import { User2 } from "lucide-react";
-
+import { Search, Menu, X } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+// header
 export default function Header() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
+
+  const navItems = [
+    { name: "Overview", href: "/" },
+    { name: "Transactions", href: "/transactions" },
+    { name: "Transfer", href: "/transfer" },
+  ];
+
   return (
-    <header className="py-4 px-2 bg-slate-800 text-slate-200">
-      <div className="container mx-auto flex justify-between">
-        <nav className="flex gap-4">
-          <Link className="hover:text-slate-400 duration-150" to="/">
-            Overview
-          </Link>
-          <Link
-            className="hover:text-slate-400 duration-150"
-            to="/transactions"
-          >
-            Transactions
-          </Link>
-          <Link className="hover:text-slate-400 duration-150" to="/transfer">
-            Transfer
-          </Link>
-        </nav>
-        <User2 />
+    <nav className="bg-slate-800 shadow-sm sm:px-2">
+      <div className="container mx-auto ">
+        <div className="flex justify-between h-16">
+          <div className="flex">
+            <div className="hidden  sm:flex sm:space-x-8">
+              {navItems.map((item) => (
+                <Link
+                  key={item.name}
+                  to={item.href}
+                  className={`flex items-center duration-75 text-slate-200 hover:text-slate-500`}
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="hidden sm:ml-6 sm:flex sm:items-center">
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <Search className="h-5 w-5 text-slate-600" aria-hidden="true" />
+              </div>
+              <Input
+                type="text"
+                placeholder="Search"
+                className="block w-full pl-10 sm:text-sm rounded-md"
+              />
+            </div>
+          </div>
+          <div className="-mr-2 flex items-center sm:hidden">
+            <Button
+              variant="ghost"
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+              className="inline-flex items-center justify-center p-2 rounded-md text-slate-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary"
+            >
+              <span className="sr-only">Open main menu</span>
+              {isMobileMenuOpen ? (
+                <X className="block h-6 w-6" aria-hidden="true" />
+              ) : (
+                <Menu className="block h-6 w-6" aria-hidden="true" />
+              )}
+            </Button>
+          </div>
+        </div>
       </div>
-    </header>
+
+      {isMobileMenuOpen && (
+        <div className="sm:hidden">
+          <div className="pt-2 pb-3 space-y-1">
+            {navItems.map((item) => (
+              <Link
+                key={item.name}
+                to={item.href}
+                className={`block pl-3 pr-4 py-2 text-base font-medium `}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <div className="pt-4 pb-3 border-t border-slate-500">
+            <div className="mt-3 px-2">
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Search
+                    className="h-5 w-5 text-slate-600"
+                    aria-hidden="true"
+                  />
+                </div>
+                <Input
+                  type="text"
+                  placeholder="Search"
+                  className="block w-full pl-10 sm:text-sm rounded-md"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </nav>
   );
 }
